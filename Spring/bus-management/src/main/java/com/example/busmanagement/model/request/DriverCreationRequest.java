3e77e1ae4e66f080852a98ba3f6d5c7731e9501b
TranDuongHoan <133374478+TranDuongHoan@users.noreply.github.com>
2023-11-20 09:06:20 +0700

    update

--- Spring/bus-management/src/main/java/com/example/busmanagement/model/request/DriverCreationRequest.java
+++ Spring/bus-management/src/main/java/com/example/busmanagement/model/request/DriverCreationRequest.java
@@ -2,17 +2,32 @@ package com.example.busmanagement.model.request;
 
 import com.example.busmanagement.statics.DriverLevel;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Data
 public class DriverCreationRequest {
-
+    @NotNull(message = "ID bắt buộc phải nhập")
+    @Min(value = 1)
     private int id;
+
+    @NotBlank(message = "Tên tài xế bắt buộc nhập")
+    @Length(max = 100, message = "Tên tài xế không được vượt quá 100 ký tự")
     private String name;
+
+    @NotBlank(message = "Địa chỉ bắt buộc nhập")
+    @Length(max = 100, message = "Địa chỉ không được vượt quá 100 ký tự")
     private String address;
+
+    @NotNull(message = "Số điện thoại bắt buộc phải nhập")
+    @Min(value = 10)
     private int phone;
+
+    @NotEmpty(message = "Hạng xe bắt buộc nhập")
+    @Size(max = 1, message = "Hạng xe không được vượt quá 1")
     private List<DriverLevel> levels;
 
 }
--- Spring/bus-management/src/main/java/com/example/busmanagement/model/request/DriverUpdateRequest.java
+++ Spring/bus-management/src/main/java/com/example/busmanagement/model/request/DriverUpdateRequest.java
@@ -14,20 +14,20 @@ public class DriverUpdateRequest {
     @Min(value = 1)
     private Integer id;
 
-    @NotBlank(message = "Tên sách bắt buộc nhập")
-    @Length(max = 100, message = "Tên sách không được vượt quá 100 ký tự")
+    @NotBlank(message = "Tên tài xế bắt buộc nhập")
+    @Length(max = 100, message = "Tên tài xế không được vượt quá 100 ký tự")
     private String name;
 
-    @NotBlank(message = "Tên tác giả bắt buộc nhập")
-    @Length(max = 100, message = "Tên tác giả không được vượt quá 100 ký tự")
+    @NotBlank(message = "Địa chỉ bắt buộc nhập")
+    @Length(max = 100, message = "Địa chỉ không được vượt quá 100 ký tự")
     private String address;
 
-    @NotNull(message = "ID bắt buộc phải nhập")
+    @NotNull(message = "Số điện thoại bắt buộc phải nhập")
     @Min(value = 10)
     private int phone;
 
-    @NotEmpty(message = "Thể loại sách bắt buộc nhập")
-    @Size(max = 10, message = "Thể loại không được vượt quá 10")
+    @NotEmpty(message = "Hạng xe bắt buộc nhập")
+    @Size(max = 1, message = "Hạng xe không được vượt quá 1")
     private List<DriverLevel> levels;
 
 }
